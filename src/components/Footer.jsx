d238e484685ded56cd40c6fb3a53c3ae178e0c24
roshdyjr <ahmedrush96@gmail.com>
2024-12-30 14:11:15 +0200

    tilt update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,8 +18,8 @@ const links = [
 
 const Footer = () => {
   return (
-    <footer className='w-screen bg-violet-300 py-4 text-black'>
-      <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
+    <footer className='w-screen bg-violet-300 py-4 text-black overflow-hidden'>
+      <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-10 md:flex-row'>
         <p className='text-center text-sm md:text-left'>
           &copy; 2023 Zentry. All rights reserved
         </p>
